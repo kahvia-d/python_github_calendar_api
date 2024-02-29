5787cd75a616389f96d306bcec8d874c53ff4abf
kahvia <110367679+kahvia-d@users.noreply.github.com>
2024-02-29 23:35:47 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -9,12 +9,20 @@ def list_split(items, n):
 def getdata(name):
     gitpage = requests.get("https://github.com/" + name)
     data = gitpage.text
+    
+    # 2023-11-22 更新正则 https://github.com/Zfour/python_github_calendar_api/issues/18
     datadatereg = re.compile(r'data-date="(.*?)" id="contribution-day-component')
-    datacountreg = re.compile(r'<span class="sr-only">(.*?) contribution')
+    datacountreg = re.compile(r'<tool-tip .*?class="sr-only position-absolute">(.*?) contribution')
+    
     datadate = datadatereg.findall(data)
     datacount = datacountreg.findall(data)
     datacount = list(map(int, [0 if i == "No" else i for i in datacount]))
 
+    # 检查datadate和datacount是否为空
+    if not datadate or not datacount:
+        # 处理空数据情况
+        return {"total": 0, "contributions": []}
+        
     # 将datadate和datacount按照字典序排序
     sorted_data = sorted(zip(datadate, datacount))
     datadate, datacount = zip(*sorted_data)
@@ -32,7 +40,9 @@ def getdata(name):
     return returndata
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        path = self.path
+        # 2024-02-21 固定用户名 https://github.com/Zfour/python_github_calendar_api/issues/20
+        # path = self.path
+        # user = path.split('?')[1]
         user = 'kahvia-d'
         data = getdata(user)
         self.send_response(200)
